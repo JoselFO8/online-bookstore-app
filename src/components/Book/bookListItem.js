5ba8f0e8440125ae38006635a19178eeca95feee
Jose <joseluisfo_@hotmail.com>
2022-11-30 22:58:55 -0500

    Icon is added in book list

--- src/components/Book/bookListItem.js
+++ src/components/Book/bookListItem.js
@@ -1,10 +1,50 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+    Image, 
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from "react-native";
+import Icon from "react-native-ionicons";
 
-export default function BookListItem({book}) {
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: "center",
+        gap: 15,
+        paddingVertical: 15,
+        paddingLeft: 15,
+        paddingRight: 15,
+
+        borderBottomWidth: 1,
+        borderBottomColor: '#E0E0E0',
+    },
+    image: {
+        height: 40,
+        width: 40,
+        resizeMode: "cover",
+    },
+    icon: {
+        color: "#000",
+        marginLeft: "auto",
+    }
+})
+
+export default function BookListItem({book, onClick}) {
     return (
-        <View>
-            <Text>{book.title}</Text>
-        </View>
+        <TouchableOpacity onPress={onClick} >
+            <View style={styles.container} >
+                <Image
+                    source={{uri: book.cover}}
+                    style={styles.image}
+                />
+                <Text>{book.title}</Text>
+                <Icon 
+                    name="add-circle"
+                    style={styles.icon}
+                />
+            </View>
+        </TouchableOpacity>
     )
 }
--- src/views/home.js
+++ src/views/home.js
@@ -5,15 +5,21 @@ import BookListItem from "../components/Book/BookListItem";
 const BOOK_LIST = [
     {
         id: 1,
-        title: 'Libro 1'
+        title: 'Libro 1',
+        cover: 'https://picsum.photos/200'
     },
     {
         id: 2,
-        title: 'Libro 2'
+        title: 'Libro 2',
+        cover: 'https://picsum.photos/200'
     },
 ]
 
 export default function Home({navigation}) {
+    function handleOnClick() {
+        navigation.navigate('Library')
+    }
+
     return (
         <View>
             <Text>Desde home</Text>
@@ -23,11 +29,17 @@ export default function Home({navigation}) {
             >
                 <Text>Ir a la libreria</Text>
             </TouchableHighlight>
+
             <View>
                 {/* Agregar cabecera mediante */}
                 <FlatList
                     data={BOOK_LIST}
-                    renderItem={({item}) => <BookListItem book={item} /> }
+                    renderItem={ ({item}) => 
+                        <BookListItem 
+                            book={item}
+                            onClick={handleOnClick}
+                        />
+                    }
                     keyExtractor={item => item.id}
                     ListHeaderComponent={
                         <View>
